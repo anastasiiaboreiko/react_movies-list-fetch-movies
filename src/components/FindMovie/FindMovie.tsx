e9ff50dbffa5a74fbd84552eaff424351205897b
Anastasiia Boreiko <kravchenkoanastasiia92@gmail.com>
2025-10-07 12:35:43 +0300

    React_Movies-List-Fetch-Movies

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,81 @@
-import React from 'react';
-import './FindMovie.scss';
+import React, { useState } from 'react';
+import cn from 'classnames';
 
-export const FindMovie: React.FC = () => {
+import './FindMovie.scss';
+import { getMovie } from '../../api';
+import { MovieCard } from '../MovieCard';
+import { MovieData } from '../../types/MovieData';
+import { ResponseError } from '../../types/ReponseError';
+import { Movie } from '../../types/Movie';
+
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const FindMovie: React.FC<Props> = ({ onAdd }) => {
+  const [query, setQuery] = useState<string>('');
+  const [foundMovie, setFoundMovie] = useState<MovieData | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const isResponseError = (
+    resp: MovieData | ResponseError,
+  ): resp is ResponseError => 'Error' in resp;
+
+  const mapMovieDataToMovie = (data: MovieData): Movie => ({
+    title: data.Title,
+    description: data.Plot,
+    imgUrl: data.Poster,
+    imdbUrl: `https://www.imdb.com/title/${data.imdbID}`,
+    imdbId: data.imdbID,
+  });
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    setError(null);
+  };
+
+  const handleSearch = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!query) {
+      return;
+    }
+
+    setError(null);
+    setFoundMovie(null);
+    setIsLoading(true);
+
+    try {
+      const response = await getMovie(query);
+
+      if (isResponseError(response)) {
+        setError(`Can't find a movie with such a title`);
+        setFoundMovie(null);
+      } else {
+        setFoundMovie(response);
+      }
+    } catch {
+      setError('Something went wrong. Please try again');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleAddButton = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    if (foundMovie) {
+      onAdd(mapMovieDataToMovie(foundMovie));
+    }
+
+    setFoundMovie(null);
+    setQuery('');
+  };
+
   return (
     <>
-      <form className="find-movie">
+      <form className="find-movie" onSubmit={handleSearch}>
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -16,13 +87,17 @@ export const FindMovie: React.FC = () => {
               type="text"
               id="movie-title"
               placeholder="Enter a title to search"
-              className="input is-danger"
+              className={cn('input')}
+              value={query}
+              onChange={handleQueryChange}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {error && (
+            <p className="help is-danger" data-cy="errorMessage">
+              {error}
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
@@ -30,28 +105,35 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={cn('button', 'is-light', { 'is-loading': isLoading })}
+              disabled={!query}
             >
-              Find a movie
+              {foundMovie ? 'Search again' : 'Find a movie'}
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {foundMovie && (
+            <div className="control">
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                style={{ color: '#000' }}
+                onClick={handleAddButton}
+              >
+                Add to the list
+              </button>
+            </div>
+          )}
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {foundMovie && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={mapMovieDataToMovie(foundMovie)} />
+        </div>
+      )}
     </>
   );
 };
